dc9023a2411e2eb65e20efe7648b73b36893439e
Blasi Eduardo <eblasi@andreani.com>
2022-08-30 21:35:30 -0300

    Primera version terminada

--- Program.cs
+++ Program.cs
@@ -17,7 +17,7 @@ namespace School_V_1
             Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new ListadoAlumno());
+            Application.Run(new Home());
         }
     }
 }
